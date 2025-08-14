56fe740371409fb192a7bd98c88de0a030a432b8
liamgd <>
2025-08-13 23:09:30 -0700

    Feature type tree generation (categories and subtypes)

--- ucla_geojson/probe.py
+++ ucla_geojson/probe.py
@@ -1,7 +1,14 @@
 import json
 
-with open("public/campus.geojson", "r") as f:
-    campus = json.load(f)["features"]
+
+def open_campus():
+    with open("public/campus.geojson", "r") as f:
+        campus = json.load(f)["features"]
+        return campus
+
+
+def probe_duplicate_centroids():
+    campus = open_campus()
     centroids_found = set()
     duplicate_centroids = set()
 
@@ -17,3 +24,16 @@ with open("public/campus.geojson", "r") as f:
         centroid = tuple(props["centroid"])
         if centroid in duplicate_centroids:
             print(props["centroid"], props["name"])
+
+
+def feature_type_tree():
+    campus = open_campus()
+    subtypes = set()
+
+    for feature in campus:
+        props = feature["properties"]
+        category = props["category"]
+        subtype = props["subtype"]
+        subtypes.add(f"{category}: {subtype}")
+
+    print(*sorted(subtypes), sep="\n")
