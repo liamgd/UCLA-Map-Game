826e206d4272c720d354ef64d19ffcd02d9a50bb
liamgd <>
2025-08-17 15:29:22 -0700

    Small fix py build

--- build_ucla_geojson.py
+++ build_ucla_geojson.py
@@ -1,7 +1,7 @@
 from ucla_geojson.main import main
 
 if __name__ == "__main__":
-    # main()
-    from ucla_geojson.probe import probe_names_categories
+    main()
+    # from ucla_geojson.probe import probe_names_categories
 
-    probe_names_categories()
+    # probe_names_categories()
